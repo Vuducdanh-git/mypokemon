0495c7f3ad7727ae1b0a7f884710ebd7d9c777af
ngbao1410 <loannguye3710@gmail.com>
2025-03-19 21:31:11 +0700

    baogay

--- core/src/main/java/k1/chuyentin/com/bao/Utils.java
+++ core/src/main/java/k1/chuyentin/com/bao/Utils.java
@@ -11,5 +11,19 @@ public class Utils {
         wordList.add("attack");
         wordList.add("defender");
         wordList.add("heal");
+        wordList.add("cat");
+        wordList.add("boarding school");
+        wordList.add("school");
+        wordList.add("cartoon");
+        wordList.add("computer  games");
+        wordList.add("book");
+        wordList.add("good morning");
+        wordList.add("play sport");
+        wordList.add("favourite");
+        wordList.add("nice to meet you");
+        wordList.add("drink");
+        wordList.add("i play games");
+        wordList.add("swimming pool");
+        wordList.add("thank you");
     }
 }
