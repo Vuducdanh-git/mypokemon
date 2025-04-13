60f0b8365a5787e07ca4e6f25d1d51c879cc49b1
nguyenbao <loannguyen3710@gmail.com>
2025-04-13 22:59:35 +0700

    them anh cho vat pham

--- core/src/main/java/k1/chuyentin/com/actors/Item.java
+++ core/src/main/java/k1/chuyentin/com/actors/Item.java
@@ -16,29 +16,40 @@ public class Item extends MyActor {
     int sodiemtangkhiclick = 1;
     public Item(float x, float y, Stage s, int type) {
         super(x, y, s);
-        textureRegion = new TextureRegion(new Texture("itemPlus1.png"));
+        textureRegion = new TextureRegion(new Texture("itemplus2.png"));
+
 
         switch (type){
-            case 1 -> textureRegion = new TextureRegion(new Texture("itemPlus1.png"));
-            case 2 -> textureRegion = new TextureRegion(new Texture("ball.png"));
+            case 1 -> textureRegion = new TextureRegion(new Texture("itemplus.png"));
+            case 2 -> textureRegion = new TextureRegion(new Texture("itemplus2.png"));
+            case 3 -> textureRegion = new TextureRegion(new Texture("itemplus3.png"));
+            case 4 -> textureRegion = new TextureRegion(new Texture("itemplus4.png"));
+            case 5 -> textureRegion = new TextureRegion(new Texture("itemplus5.png"));
+            case 6 -> textureRegion = new TextureRegion(new Texture("itemplus6.png"));
+            case 7 -> textureRegion = new TextureRegion(new Texture("itemplus7.png"));
+            case 8 -> textureRegion = new TextureRegion(new Texture("itemplus8.png"));
+            case 9 -> textureRegion = new TextureRegion(new Texture("itemplus9.png"));
+            case 10 -> textureRegion = new TextureRegion(new Texture("itemplus10.png"));
+
         }
 
-        setSize(textureRegion.getRegionWidth(), textureRegion.getRegionHeight());
+        setSize(64, 64);
 
-        giatienclick = (int) (100* (Math.pow(type, type)));
+        giatienclick = (int) (100* (Math.pow(type, type))/type/type/type);
 
         addListener(new ClickListener() {
 
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 if (Master.money >= giatienclick){
+                    Master.money -= giatienclick;
                     sodiemtangkhiclick = giatienclick/50;
                     Master.plus += sodiemtangkhiclick;
                 }
             }
         });
         setPosition(Gdx.graphics.getWidth(), y);
-        addAction(Actions.moveTo(x, y, 0.8f));
+        addAction(Actions.moveTo(x, y, 0.5f));
     }
 
 }
