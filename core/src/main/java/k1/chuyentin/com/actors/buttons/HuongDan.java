4434eef6bb0b5e64d45e311278203bf702785c19
Vuducdanh-git <vuducdanh1202@gmail.com>
2025-04-14 08:24:03 +0700

    complete100

--- core/src/main/java/k1/chuyentin/com/actors/buttons/HuongDan.java
+++ core/src/main/java/k1/chuyentin/com/actors/buttons/HuongDan.java
@@ -2,15 +2,23 @@ package k1.chuyentin.com.actors.buttons;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import k1.chuyentin.com.actors.MyActor;
 
-public class HuongDan extends MyActor {
+public class HuongDan extends Actor {
+    TextureRegion textureRegion;
     public HuongDan(float x, float y, Stage s) {
-        super(x, y, s);
         textureRegion = new TextureRegion(new Texture("hd.png"));
-        setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()-80);
+        setPosition(x, y);
+    }
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        batch.setColor(getColor().r, getColor().g, getColor().b, getColor().a * parentAlpha);
+        batch.draw(textureRegion, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+        batch.setColor(1, 1, 1, 1); // Reset lại màu batch để tránh ảnh hưởng đến các Actor khác}
     }
 }
