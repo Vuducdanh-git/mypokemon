4d89b42f967b83578db328b419d5505c911fd64f
Vuducdanh-git <vuducdanh1202@gmail.com>
2025-05-01 20:20:18 +0700

    ss

--- core/src/main/java/k1/chuyentin/com/screens/WordListScreen.java
+++ core/src/main/java/k1/chuyentin/com/screens/WordListScreen.java
@@ -5,20 +5,27 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import k1.chuyentin.com.StartGame;
 import k1.chuyentin.com.actors.Bth;
+import k1.chuyentin.com.actors.FloatingWords;
+import k1.chuyentin.com.actors.Wlsword;
 import k1.chuyentin.com.actors.buttons.NutMenu;
 import k1.chuyentin.com.enums.ButtonType;
 
 public class WordListScreen implements Screen {
     Stage stage;
     Bth exit;
+    Wlsword wlsword;
+    int repet =0;
+    int y =25;
+    int x =0;
 
-    WordListScreen(StartGame game){
+    public WordListScreen(StartGame game){
         stage = new Stage();
 
         exit = new Bth(stage, Gdx.graphics.getWidth()-100 ,50,100,50);
@@ -27,7 +34,15 @@ public class WordListScreen implements Screen {
                 game.setScreen(new Master(game));
             }
         });
+        if (!Master.wordSkills.isEmpty()) {
+            String words = Master.wordSkills.get(repet);
+            new FloatingWords(0, 0, stage, words);
+
+            String wordsVN = Master.wordSkillsVN.get(repet);
+            new FloatingWords(0, 0, stage, wordsVN);
+        }
         stage.addActor(exit);
+
     }
 
     @Override
@@ -40,6 +55,17 @@ public class WordListScreen implements Screen {
         ScreenUtils.clear(Color.BLACK);
         stage.act();
         stage.draw();
+        if (!Master.wordSkills.isEmpty()) {
+            String words = Master.wordSkills.get(repet);
+            new FloatingWords(x, Gdx.graphics.getHeight()-y, stage, words);
+
+            String wordsVN = Master.wordSkillsVN.get(repet);
+            new FloatingWords(x+Master.wordSkills.get(repet).length()*5, Gdx.graphics.getHeight()-y, stage, wordsVN);
+            repet++;
+            x +=Master.wordSkillsVN.get(repet).length()*5;
+        }
+
+
     }
 
     @Override
